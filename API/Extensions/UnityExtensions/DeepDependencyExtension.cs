a196e25266eb02122d7a47e1d62c55a5b9af77cf
ydnakdivad <davidkandy98@gmail.com>
2021-04-30 21:05:08 +0100

    ..

---
+++ API/Extensions/UnityExtensions/DeepDependencyExtension.cs
@@ -0,0 +1,77 @@
+using Mono.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NLog;
+using System.Reflection;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Builder;
+using Unity.Extension;
+using Unity.Strategies;
+using Microsoft.Extensions.Logging;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace API.Extensions.UnityExtensions
+{
+    #region Extension
+    internal class DeepDependencyExtension : UnityContainerExtension
+    {
+        protected override void Initialize()
+        {
+            Context.Strategies.Add(new DeepDependencyStrategy(),
+                UnityBuildStage.Initialization);
+        }
+    }
+    #endregion
+
+    #region Attributes
+    /// <summary>
+    /// Automatically resolves a property on initialization.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DeepDependency : Attribute { }
+    #endregion
+
+    #region Strategy
+    class DeepDependencyStrategy : BuilderStrategy
+    {
+
+        public static Logger Log { get; } = LogManager.GetCurrentClassLogger();
+
+
+        public override void PostBuildUp(ref BuilderContext context)
+        {
+            if (context.Type == typeof(object)) return;
+            var container = context.Container;
+
+            var properties = context.Existing.GetType().GetProperties(
+                BindingFlags.Public | BindingFlags.NonPublic |
+                BindingFlags.GetProperty | BindingFlags.SetProperty |
+                BindingFlags.Instance)
+                .Where(x => x.GetCustomAttributes(typeof(DeepDependency), false)
+                .Any());
+
+            foreach (var prop in properties)
+            {
+                object value = null;
+
+                // Resolve ILogger by simply creating it based on the current class
+                if (!prop.PropertyType.IsGenericType && prop.PropertyType.IsAssignableTo<ILogger>())
+                    value = container.TryResolve<ILoggerFactory>().CreateLogger(context.Type);
+                else value = context.Container.TryResolve(prop.PropertyType);
+
+                try
+                {
+                    if (prop.CanWrite) prop.SetValue(context.Existing, value);
+                    else prop.GetBackingField().SetValue(context.Existing, value);
+                }
+                catch (Exception ex)
+                {
+                    Log.Error($"An error occured during deep property resolutiion\n{ex}");
+                }
+            }
+        }
+    }
+    #endregion
+}
---
+++ API/Extensions/UnityExtensions/UnityContainerHelper.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Lifetime;
+
+namespace API.Extensions.UnityExtensions
+{
+    /// <summary>
+    /// Extensions methods to extend and facilitate the usage of <see cref="IUnityContainer"/>.
+    /// </summary>
+    public static class UnityContainerHelper
+    {
+        /// <summary>
+        /// Returns whether a specified type has a type mapping registered in the container.
+        /// </summary>
+        /// <param name="container">The <see cref="IUnityContainer"/> to check for the type mapping.</param>
+        /// <param name="type">The type to check if there is a type mapping for.</param>
+        /// <returns><see langword="true"/> if there is a type mapping registered for <paramref name="type"/>.</returns>
+        /// <remarks>In order to use this extension method, you first need to add the
+        /// <see cref="IUnityContainer"/> extension to the extension to the UnityBootstrapperExtension.
+        /// </remarks>        
+        public static bool IsTypeRegistered(this IUnityContainer container, Type type)
+        {
+            return container.IsRegistered(type);
+        }
+
+        /// <summary>
+        /// Utility method to try to resolve a service from the container avoiding an exception if the container cannot build the type.
+        /// </summary>
+        /// <param name="container">The cointainer that will be used to resolve the type.</param>
+        /// <typeparam name="T">The type to resolve.</typeparam>
+        /// <returns>The instance of <typeparamref name="T"/> built up by the container.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter")]
+        public static T TryResolve<T>(this IUnityContainer container)
+        {
+            object result = TryResolve(container, typeof(T));
+            if (result != null)
+            {
+                return (T)result;
+            }
+            return default(T);
+        }
+
+        /// <summary>
+        /// Utility method to try to resolve a service from the container avoiding an exception if the container cannot build the type.
+        /// </summary>
+        /// <param name="container">The cointainer that will be used to resolve the type.</param>
+        /// <param name="typeToResolve">The type to resolve.</param>
+        /// <returns>The instance of <paramref name="typeToResolve"/> built up by the container.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        public static object TryResolve(this IUnityContainer container, Type typeToResolve)
+        {
+            try
+            {
+                return container.Resolve(typeToResolve);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        public static void RegisterControllers(this IUnityContainer container)
+        {
+            var controllers = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(type => typeof(ControllerBase).IsAssignableFrom(type));
+
+            foreach (var controller in controllers)
+                container.RegisterType(controller, new TransientLifetimeManager());
+        }
+
+        public static void RegisterTransient<TTFrom, TTo>(this IUnityContainer container) where TTo : TTFrom
+            => container.RegisterType<TTFrom, TTo>(new TransientLifetimeManager());
+
+        public static void RegisterTransient<T>(this IUnityContainer container)
+            => container.RegisterType<T>(new TransientLifetimeManager());
+
+        public static void RegisterScoped<TTFrom, TTo>(this IUnityContainer container) where TTo : TTFrom
+            => container.RegisterType<TTFrom, TTo>(new HierarchicalLifetimeManager());
+
+        public static void RegisterScoped<T>(this IUnityContainer container)
+            => container.RegisterType<T>(new HierarchicalLifetimeManager());
+    }
+}
